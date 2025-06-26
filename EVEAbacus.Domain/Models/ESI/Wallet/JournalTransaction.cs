f42f763402ea1a7df217c78f59ee9f3984e11093
Misfit Initiative <210905764+CLangCodes@users.noreply.github.com>
2025-06-26 17:51:47 -0400

    more non-null value corrections in form of setting empty string

--- EVEAbacus.Domain/Models/ESI/Wallet/JournalTransaction.cs
+++ EVEAbacus.Domain/Models/ESI/Wallet/JournalTransaction.cs
@@ -11,13 +11,13 @@ namespace EVEAbacus.Domain.Models.ESI.Wallet
         public float Amount { get; set; }
         public float Balance { get; set; }
         public long ContextId { get; set; }
-        public string ContextIDType { get; set; }
-        public string Date { get; set; }
-        public string Description { get; set; }
+        public string ContextIDType { get; set; } = string.Empty;
+        public string Date { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public int FirstPartyId { get; set; }
         public long Id { get; set; }
-        public string Reason { get; set; }
-        public string RefType { get; set; }
+        public string Reason { get; set; } = string.Empty;
+        public string RefType { get; set; } = string.Empty;
         public int SecondPartyId { get; set; }
         public float Tax { get; set; }
         public int TaxReceiverId { get; set; }
--- EVEAbacus.Domain/Models/ESI/Wallet/Transaction.cs
+++ EVEAbacus.Domain/Models/ESI/Wallet/Transaction.cs
@@ -9,7 +9,7 @@ namespace EVEAbacus.Domain.Models.ESI.Wallet
     public class Transaction
     {
         public int ClientId { get; set; }
-        public string Date { get; set; }
+        public string Date { get; set; } = string.Empty;
         public bool IsBuy { get; set; }
         public bool IsPersonal { get; set; }
         public long JournalRefId { get; set; }
