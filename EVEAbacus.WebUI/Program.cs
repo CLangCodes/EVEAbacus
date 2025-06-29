4d9d803e99f76590941a34c06f913c6da2e6cc54
Misfit Initiative <210905764+CLangCodes@users.noreply.github.com>
2025-06-29 13:20:14 -0400

    allowing CORS for blazor.eveabacus.com

--- EVEAbacus.WebUI/Program.cs
+++ EVEAbacus.WebUI/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("https://blazor.eveabacus.com, http://localhost:3000")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
