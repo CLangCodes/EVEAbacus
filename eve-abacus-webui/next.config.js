b1ecf8faede16c8ca9b2cf92fe6f09222485877a
Misfit Initiative <210905764+CLangCodes@users.noreply.github.com>
2025-06-30 18:11:22 -0400

    progress update1

--- eve-abacus-webui/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable standalone output for production deployment (industry standard)
-  output: 'standalone',
-  /* config options here */
-};
-
-module.exports = nextConfig;
